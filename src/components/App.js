43ead4367b764f9d5ed9a6c223551ebb7495e476
jwfrasier <frasier.joseph@gmail.com>
2018-12-13 09:46:55 -0600

    got the fetch working

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-import '../App.css';
-import Nav from './Nav'
+import React, { Component } from "react";
+import "../App.css";
+import Nav from "./Nav";
+const URL = "http://localhost:3001/hogs";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pigs: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(pigs) {
+        console.log(pigs);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-          < Nav />
-
+        <Nav />
       </div>
-    )
+    );
   }
 }
 
